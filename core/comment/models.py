88983002a73c472abe9640937d77b1125724266b
cactus1386 <cactusinjast@gmail.com>
2023-12-24 13:31:52 -0800

    account model basic

--- core/comment/models.py
+++ core/comment/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
     # post = models.ForeignKey(Product, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
