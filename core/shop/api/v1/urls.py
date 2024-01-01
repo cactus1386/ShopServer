1402231d49519be3b28a7526fbbc5975fce7324f
cactus1386 <cactusinjast@gmail.com>
2024-01-01 12:36:07 -0800

    generic view

--- core/shop/api/v1/urls.py
+++ core/shop/api/v1/urls.py
@@ -5,5 +5,5 @@ app_name = 'api-v1'
 
 urlpatterns = [
     path('products/', views.ProductList.as_view(), name='product-list'),
-    path('products/<int:id>/', views.ProductDetail.as_view(), name='product-detail'),
+    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
 ]
--- core/shop/api/v1/views.py
+++ core/shop/api/v1/views.py
@@ -1,37 +1,54 @@
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly,
+    IsAdminUser,
+    IsAuthenticated,
+)
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import ProductSerializer
 from ...models import Product
-from rest_framework.generics import GenericAPIView, ListAPIView, ListCreateAPIView
+from rest_framework.generics import (
+    ListAPIView,
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+
 
 class ProductList(ListCreateAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
-    
-class ProductDetail(APIView):
-    serializer_class = ProductSerializer
 
-    def get(self, request, id):
-        product = Product.objects.get(pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    
-    def put(self, request, id):
-        product = Product.objects.get(pk=id)
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
-    def delete(self, request, id):
-        product = Product.objects.get(pk=id)
-        product.delete()
-        return Response({'detail':'Item delete successfully'}, status=status.HTTP_204_NO_CONTENT)
-    
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+
+
+# class ProductDetail(APIView):
+#     serializer_class = ProductSerializer
+
+#     def get(self, request, id):
+#         product = Product.objects.get(pk=id)
+#         serializer = ProductSerializer(product)
+#         return Response(serializer.data)
+
+#     def put(self, request, id):
+#         product = Product.objects.get(pk=id)
+#         serializer = ProductSerializer(product, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+
+#     def delete(self, request, id):
+#         product = Product.objects.get(pk=id)
+#         product.delete()
+#         return Response({'detail':'Item delete successfully'}, status=status.HTTP_204_NO_CONTENT)
+
 
 # # The class for get and create products
 # class ProductList(APIView):
@@ -43,7 +60,7 @@ class ProductDetail(APIView):
 #         products = Product.objects.all()
 #         serializer = ProductSerializer(products, many=True)
 #         return Response(serializer.data)
-    
+
 #     # Post function
 #     def post(self, request):
 #         serializer = ProductSerializer(data=request.data)
