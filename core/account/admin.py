6ba22ae5483a2152705d074e982d3be8aa1231f9
cactus1386 <cactusinjast@gmail.com>
2024-01-10 17:37:28 -0800

    add Profile model

--- core/account/admin.py
+++ core/account/admin.py
@@ -1,14 +1,31 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Profile
 from django.contrib.auth.admin import UserAdmin
+
 
 # Register your models here.
 class CustomAdmin(UserAdmin):
     model = User
-    list_display = ('email', 'is_staff', 'is_active')
-    list_filter = ('email', 'is_staff', 'is_active')
-    searching_fields = ('email', )
-    ordering = ('email',)
+    list_display = ("email", "is_staff", "is_active")
+    list_filter = ("email", "is_staff", "is_active")
+    searching_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
+        ("group permissions", {"fields": ("groups", "user_permissions")}),
+        ("login date", {"fields": ("last_login",)}),
 
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", 'is_superuser'
+            )}
+        ),
+    )
 
 admin.site.register(User, CustomAdmin)
+admin.site.register(Profile)
--- core/account/models.py
+++ core/account/models.py
@@ -32,7 +32,6 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
-        extra_fields.setdefault("is_verified", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("Superuser must have is_staff=True."))
@@ -45,6 +44,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
     # is_verified = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
@@ -52,5 +52,21 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
+    objects = UserManager()
+
     def __str__(self):
         return self.email
+    
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        self.user.email
