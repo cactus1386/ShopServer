586de93c651e48c086f45b90db822f905c49f130
cactus1386 <cactusinjast@gmail.com>
2024-07-03 15:13:56 -0700

    add cart

---
+++ core/cart/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from shop.api.v1.serializers import ProductSerializer
+from models import Cart, CartItem
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = CartItem
+        fields = '__all__'
+
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = ('id', 'user', 'created_at', 'items', 'total_price')
+
+    def get_total_price(self, obj):
+        return obj.get_total_price()
