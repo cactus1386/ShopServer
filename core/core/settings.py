ba42be4435dd2786de3651b1b241388d65f4aec3
cactus1386 <cactusinjast@gmail.com>
2024-09-08 21:21:06 -0700

    add logo model

--- core/core/settings.py
+++ core/core/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     "djoser",
     'cart',
     'slider',
+    'navbar',
 ]
 
 MIDDLEWARE = [
---
+++ core/navbar/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Logo
+
+# Register your models here.
+admin.site.register(Logo)
